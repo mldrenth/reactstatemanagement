a80fbab2eb7c719b8829038e28b93e5c1ea2b1b8
Marijke Drenth <drenth.marijke@googlemail.com>
2023-01-08 21:29:29 +0000

    Initial commit

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MealsList from '../components/MealsList/MealsList';
+import { MEALS } from '../data/dummy-data';
+import {useSelector} from 'react-redux'
+// import { FavoritesContext } from '../store/context/favorites-context';
+
+function FavoritesScreen() {
+  // const favoriteMealsCtx = useContext(FavoritesContext)
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+
+  const favoriteMeals = MEALS.filter(meal => favoriteMealIds.includes(meal.id))
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet!</Text>
+      </View>
+    )
+  }
+  return <MealsList items={favoriteMeals} />
+}
+
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+})
